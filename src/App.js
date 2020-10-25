0b9180ded0a690a407cb186908f364a92264aabd
RyanC34 <Ryan.chan07@icloud.com>
2020-10-25 08:28:55 -0400

    Styled the layou(of the header and body), created new container for body

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import CreateButton from './components/CreateButton.js'
 import Header from './components/Header.js'
+import Body from "./components/Body.js"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header/>
-        <CreateButton/>
+        <Body/>
       </header>
     </div>
   );
---
+++ src/components/Body.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CreateButton from './CreateButton'
+import '../App.css';
+export default function Body() {
+    return (
+        <div className="Body">
+            <CreateButton/>
+        </div>
+    )
+}
